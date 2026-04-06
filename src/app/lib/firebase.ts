abb0cf5d7912717f6ae38daa3d67cfbe06eace31
isabuitrago1223 <buitragoisa1223@gmail.com>
2026-04-06 10:23:50 -0500

    Instale la dependencia firebase framer-motion, para que el sistema de login pueda funcional con facilidad

---
+++ src/app/lib/firebase.ts
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+
+const firebaseConfig = {
+  apiKey: "TU_API_KEY",
+  authDomain: "TU_PROYECTO.firebaseapp.com",
+  projectId: "TU_PROYECTO",
+  storageBucket: "TU_PROYECTO.appspot.com",
+  messagingSenderId: "XXXX",
+  appId: "XXXX"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
